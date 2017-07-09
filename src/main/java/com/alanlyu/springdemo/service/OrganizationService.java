0d3a71ffc5c989bcd152d5aa42453a27ab0d1cf9
AlanLyu1992 <lyu.y@wustl.edu>
2017-07-09 11:44:39 -0500

    jndi

---
+++ src/main/java/com/alanlyu/springdemo/service/OrganizationService.java
@@ -0,0 +1,22 @@
+package com.alanlyu.springdemo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanlyu.springdemo.dao.OrganizationDao;
+import com.alanlyu.springdemo.domain.Organization;
+
+@Service
+public class OrganizationService {
+	
+	@Autowired
+	private OrganizationDao organizationDao;
+	
+	public List<Organization> getOrgList(){
+		List<Organization> orgList = organizationDao.getAllOrganizations();
+		return orgList;
+	}
+}
